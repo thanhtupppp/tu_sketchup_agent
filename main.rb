f512f94ac7fc48dd1199633ee53e3d755f1668e7
thanhtupppp <devthanhtu@gmail.com>
2026-09-12 12:52:53 +0700

    feat: load plan checkpoint module

--- main.rb
+++ main.rb
@@ -18,6 +18,7 @@ module TuSketchupAgent
     "core/idempotency",
     "core/recovery",
     "core/plan",
+    "core/plan_checkpoint",
     "core/plan_executor",
     "core/router",
     "core/server",
